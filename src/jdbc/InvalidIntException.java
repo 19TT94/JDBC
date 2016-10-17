c5415b6ade76b9745b600ff37fde5c8c88039306
Taylor Tobin <19tt94@gmail.com>
2016-10-16 22:19:24 -0700

    added exception class

---
+++ src/jdbc/InvalidIntException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jdbc;
+
+/**
+ *
+ * @author TT
+ */
+class InvalidIntException extends Exception {
+    public InvalidIntException(String message) {
+        super(message);
+    }   
+}
